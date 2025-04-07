0cf74781236918090c583a31d9a38221ae6e6aa8
siog <siog@LAPTOP-545TJ6PE>
2025-04-07 16:59:50 +0100

    Add for loop

--- StringReverser/src/ie/atu/sw/StringReverser.java
+++ StringReverser/src/ie/atu/sw/StringReverser.java
@@ -1,9 +1,12 @@
 package ie.atu.sw;
 
 public class StringReverser {
-	
 	public String reverse(String s) {
-		return null;
+		
+		for (int i = s.length() - 1; i >= 0; i--) {
+			
+		}
+		
 	}
 	
 	public static void main(String[] args) {
