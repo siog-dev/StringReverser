8b92706aa8d01a93aabfc348c253015d5cbd10a5
siog <siog@LAPTOP-545TJ6PE>
2025-04-07 17:01:32 +0100

    Add print statement using charAt()

--- StringReverser/src/ie/atu/sw/StringReverser.java
+++ StringReverser/src/ie/atu/sw/StringReverser.java
@@ -4,9 +4,10 @@ public class StringReverser {
 	public String reverse(String s) {
 		
 		for (int i = s.length() - 1; i >= 0; i--) {
-			
+			System.out.println(s.charAt(i));
 		}
 		
+		return null;
 	}
 	
 	public static void main(String[] args) {
