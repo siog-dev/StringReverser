20888aaff43f0550c1519d6f38fb8d5021bde2e6
siog <siog@LAPTOP-545TJ6PE>
2025-04-07 16:56:52 +0100

    Add constructor, create new instance of StringReverser and add logic

--- StringReverser/src/ie/atu/sw/StringReverser.java
+++ StringReverser/src/ie/atu/sw/StringReverser.java
@@ -1,5 +1,14 @@
 package ie.atu.sw;
 
 public class StringReverser {
-
+	
+	public String reverse(String s) {
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		StringReverser sr = new StringReverser();
+		String s = sr.reverse("The Quick Brown Fox");
+		System.out.println(s);
+	}
 }
