928aefdd2ae15ca45b3d0527ce94a9baf2284c03
siog <siog@LAPTOP-545TJ6PE>
2025-04-07 17:04:40 +0100

    Change method of printing the result

--- StringReverser/src/ie/atu/sw/StringReverser.java
+++ StringReverser/src/ie/atu/sw/StringReverser.java
@@ -3,11 +3,12 @@ package ie.atu.sw;
 public class StringReverser {
 	public String reverse(String s) {
 		
+		String t = "";
 		for (int i = s.length() - 1; i >= 0; i--) {
-			System.out.println(s.charAt(i));
+			t = t + s.charAt(i); //Don't do this inside a loop, inefficient
 		}
 		
-		return null;
+		return t;
 	}
 	
 	public static void main(String[] args) {
